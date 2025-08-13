2ac68d0032d4566198693fa31879ce88a17a0ea5
reggaebill <aarongerhart7@gmail.com>
2025-08-13 11:46:08 -0400

    add titles and scene transitions

---
+++ Scenes/title_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+func _on_start_button_pressed() -> void:
+	SceneTransitions.load_scene("Scenes/main.tscn")
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
